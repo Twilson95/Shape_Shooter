eee6ea03cd3088088e7c41ea0d95467cf4935462
Twilson95 <55410123+Twilson95@users.noreply.github.com>
2026-04-17 18:13:51 +0100

    Fix missing Unity Services Core exception namespace

--- Assets/Scripts/GooglePlayServices/GoogleSignIn.cs
+++ Assets/Scripts/GooglePlayServices/GoogleSignIn.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using Unity.Services.Authentication;
+using Unity.Services.Core;
 using UnityEngine;
 
 public class GoogleSignIn : MonoBehaviour
